b25b1d4be77e33b6a1fb16d58f7af1fbec5c0f0b
Hayk Malkhasyan <haykmalk@yahoo.com>
2019-11-04 19:43:40 +0400

    New entry

---
+++ Hi Hayk/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hi_Hayk
+{
+    class Program
+    {
+        static int Add(int x , int y)
+        {
+            int sum = x + y;
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add(5,4));
+        }
+    }
+}
