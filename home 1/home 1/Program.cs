00085b7775c98e38446de7b17d3faa0acddd6579
Hayk Malkhasyan <haykmalk@yahoo.com>
2019-11-07 13:43:28 +0400

    home 1

---
+++ home 1/home 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_1
+{
+    class Program
+    {
+        static void  ChangeType(double[]first)
+        {
+            int[] last = new int[first.Length];
+            for(int i = 0; i <last.Length;i++)
+            {
+                last[i] = (int)(first[i]);
+                Console.Write(last[i] + ",");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            double[] a = { 1.2, 5.8, 3.2 };
+            ChangeType(a);
+            
+        }
+    }
+}
