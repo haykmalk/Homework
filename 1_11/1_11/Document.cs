84b0b95cf46980ce300f26a323e1da012b8d1133
Hayk Malkhasyan <haykmalk@yahoo.com>
2020-02-16 17:16:40 +0400

    kk

---
+++ 1_11/1_11/Document.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_11
+{
+    class Document
+    {
+        Title title = null;
+        Body body = null;
+        Footer footer = null;
+        public void Initialize()
+        {
+            this.title = new Title();
+            this.body = new Body();
+            this.footer = new Footer();
+        }
+        public Document(string title)
+        {
+            Initialize();
+            this.title.Content = title;
+        }
+        public void Show()
+        {
+            this.title.Show();
+            this.body.Show();
+            this.footer.Show();
+
+        }
+        public string Body
+        {
+            set
+            {
+                this.body.Content = value;
+            }
+        }
+        public string Footer
+        {
+            set
+            {
+                this.footer.Content = value;
+            }
+        }
+    }
+}
---
+++ 1_11/1_11/Title.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_11
+{
+    class Title
+    {
+        private string content;
+        public string Content
+        {
+            private get
+            {
+                if (content != null)
+                {
+                    return content;
+                }
+                else
+                {
+                    return "missing content";
+                }
+            }
+            set
+            {
+                content = value;
+            }
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(Content);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
